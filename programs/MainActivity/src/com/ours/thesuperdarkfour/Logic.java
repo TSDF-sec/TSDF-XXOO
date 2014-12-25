ddfc56a2ed239133343e03045ce6f04838c117b0
TSDF-sec <1021265613@qq.com>
2014-12-25 21:40:49 +0800

    用户文件存取与Logic类
    用户文件存取与Logic类

--- programs/MainActivity/src/com/ours/thesuperdarkfour/Logic.java
+++ programs/MainActivity/src/com/ours/thesuperdarkfour/Logic.java
@@ -1,12 +1,55 @@
 package com.ours.thesuperdarkfour;
-/**
- * Logic类：负责处理所有和逻辑相关的事项，并为界面提供操作接口
- * @author GaoXiang
- *
- */
+
+import com.ours.thesuperdarkfour.User.Gender;
+
 public class Logic {
 	
-	Logic() {
+	private User user = new User();
+	
+	//建立新用户并存储至TXT，需要指定ID、名字、年龄、性别、密码
+	public void createNewUser(int newID, String newName, int newAge, Gender newGender, String newPasscode) {
+		user.userID = newID;
+		user.userName = newName;
+		user.userAge = newAge;
+		user.userGender = newGender;
+		user.userPasscode = newPasscode;
+		user.saveUserInfoToFile();
+	}
+
+	//加载TXT中存储的用户信息
+	public void loadUserInfo() {
+		user.loadUserInfoFromFile();
+	}
+	
+	//返回用户信息
+	public User getUser() {
+		return this.user;
+	}
+	
+	//唯一性
+	private static Logic _logic;
+	public static Logic getLogic() {
+		return _logic;
+	}
+	public static void setInitLogic( Logic logic ) 
+	{
+		_logic = logic;
+	}
+	
+	//也是测试用的main函数
+	public static void main(String[] args) {
+		Logic logic = new Logic();
+		Logic.setInitLogic(logic);
+		
+//		Logic.getLogic().createNewUser(222, "GX", 88, Gender.MALE, "wangtongxue");
+		Logic.getLogic().loadUserInfo();
+		
+		System.out.println("Current User is " + Logic.getLogic().user.userName);
+		System.out.println("His/Her ID is " + Logic.getLogic().user.userID);
+		System.out.println("His/Her Age is " + Logic.getLogic().user.userAge);
+		System.out.println("His/Her PassCode is " + Logic.getLogic().user.userPasscode);
+		
 		
 	}
+
 }
--- programs/MainActivity/src/com/ours/thesuperdarkfour/User.java
+++ programs/MainActivity/src/com/ours/thesuperdarkfour/User.java
@@ -1,10 +1,22 @@
 package com.ours.thesuperdarkfour;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class User {
 	//basic information
-	public short userID;
+	public int userID;
 	public String userName;
-	public boolean userGender;
+	public int userAge;
+	public enum Gender{MALE,FEMALE,SHEMALE};
+	public Gender userGender;
+	public String userPasscode;
 	
 	//5 dimensions
 	public Dimension userDimension;
@@ -16,4 +28,69 @@ public class User {
 	public User [] friendlist;
 	
 	//accomplishments
+	
+	//constructors
+	public User(){};
+	
+	public User(int newID, String newName, int newAge, Gender newGender, String newPasscode) {
+		this.userID = newID;
+		this.userName = newName;
+		this.userAge = newAge;
+		this.userGender = newGender;
+		this.userPasscode = newPasscode;
+	}
+	
+	//functions
+	public void saveUserInfoToFile() {
+		BufferedWriter bufferedWriter = null;
+		try {
+			File file = new File("User.txt");
+			bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true), "UTF-8")); // 指定编码格式，以免读取时中文字符异常
+			bufferedWriter.append(String.valueOf(this.userID));
+			bufferedWriter.newLine();
+			bufferedWriter.append(this.userName);
+			bufferedWriter.newLine();
+			bufferedWriter.append(String.valueOf(this.userAge));
+			bufferedWriter.newLine();
+			bufferedWriter.append(this.userGender.name());
+			bufferedWriter.newLine();
+			bufferedWriter.append(this.userPasscode);
+			bufferedWriter.newLine();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (bufferedWriter != null) {
+				try {
+					bufferedWriter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public void loadUserInfoFromFile() {
+		String filePath = "User.txt";
+		BufferedReader bufferedReader = null;
+		try {
+			bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "UTF-8")); // 指定读取文件的编码格式，要和写入的格式一致，以免出现中文乱码,
+			this.userID = Integer.valueOf(bufferedReader.readLine());
+			this.userName = bufferedReader.readLine();
+			this.userAge = Integer.valueOf(bufferedReader.readLine());
+			this.userGender = Gender.valueOf(bufferedReader.readLine());
+			this.userPasscode = bufferedReader.readLine();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bufferedReader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
 }
